5d577e908e1980f44d86e1a4460a204fbb218c1e
DESKTOP-RPRBQ5K\nicol <nicolai.calincov@gmail.com>
2022-02-02 09:27:18 +0200

    Added solution for PermMissingElem task.

---
+++ PermMissingElem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ObjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] {2,3,1,5};
+            Console.WriteLine(solution(A));
+        }
+
+        public static int solution(int[] A)
+        {
+            int n = A.Length + 1;
+            int sumOfArrayElements = (n * (1 + n)) / 2;
+            int missingValue = sumOfArrayElements - A.Sum();
+            return missingValue;
+        }
+    }
+}
