749bee6f802077f7a0c5f2b97dc4c790e2289f71
DESKTOP-RPRBQ5K\nicol <nicolai.calincov@gmail.com>
2022-02-02 09:23:03 +0200

    Added solution for FrogJmp task.

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ObjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(10, 85, 30));
+        }
+
+        public static int Solution(int X, int Y, int D)
+        {
+            int step;
+            if ((Y - X) % D == 0)
+                step = (Y - X) / D;
+            else
+                step = (Y - X) / D + 1;
+            return step;
+        }
+    }
+}
