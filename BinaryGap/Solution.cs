d46832553b38a83d597aa4408d7b7145e41c9c78
DESKTOP-RPRBQ5K\nicol <nicolai.calincov@gmail.com>
2022-02-02 16:48:35 +0200

    Modified solution for BinaryGap according to the comment.

---
+++ BinaryGap/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace ObjectApp;
+
+class Solution
+{
+    public int BinaryGap(int Number)
+    {
+        int longest = 0;
+        int curCount = 0;
+        bool firstFound = false;
+
+        for (int i = 0; i < sizeof(int) * 8; i++)
+        {
+            int bit = (Number >> i) & 1;
+
+            if (bit == 1)
+            {
+                firstFound = true;
+                
+                if (curCount >= longest)
+                {
+                    longest = curCount;
+                    curCount = 0;
+                }
+                else
+                {
+                    curCount = 0;
+                }
+            }
+            else
+            {
+                if (firstFound)
+                {
+                    curCount++;
+                }
+            }
+        }
+
+        return longest;
+    }
+}
