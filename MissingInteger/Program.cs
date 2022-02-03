dcf21f5dbfc2fba29d6c2f6629aefab9de0a8bb1
DESKTOP-RPRBQ5K\nicol <nicolai.calincov@gmail.com>
2022-02-03 14:10:07 +0200

    Added solution for MaxInteger task.

---
+++ MissingInteger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ObjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayElements = new int[] {1, 3, 6, 4, 1, 2};
+            Console.WriteLine(MissingInteger(arrayElements));
+        }
+
+        public static int MissingInteger(int[] arrayElements)
+        {
+            int arrayLength = arrayElements.Length;
+            int minValueFound = 1;
+            Array.Sort(arrayElements);
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                if (arrayElements[i] <= 0)
+                {
+                    continue;
+                }
+                
+                else if (arrayElements[i] == minValueFound)
+                {
+                    minValueFound++;
+                }
+            }
+
+            return minValueFound;
+        }
+    }
+}
