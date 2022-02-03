dd80812b405851a08ca9b2bbe1eab58ce06f0b00
DESKTOP-RPRBQ5K\nicol <nicolai.calincov@gmail.com>
2022-02-03 11:27:25 +0200

    Added solution for MaxCounters task, but its performance is low, only 40%.

---
+++ MaxCounters/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ObjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayElements = new int[] {3, 4, 4, 6, 1, 4, 4};
+            int intNumber = 5;
+            Console.WriteLine(MaxCounter(intNumber, arrayElements));
+        }
+
+        public static int[] MaxCounter(int intNumber, int[] arrayElements)
+        {
+            int[] arrayCounters = new int[intNumber];
+            int arrayElementsLength = arrayElements.Length;
+
+            for (int i = 0; i < arrayElementsLength; i++)
+            {
+                int counter = arrayElements[i];
+                
+                if (counter >= 1 && counter <= intNumber)
+                {
+                    arrayCounters[counter - 1] = arrayCounters[counter - 1] + 1;
+                }
+
+                if (counter == intNumber + 1)
+                {
+                    int tempMaxCounter = arrayCounters.Max();
+                    int arrayCountersLength = arrayCounters.Length;
+                
+                    for (int j = 0; j < arrayCountersLength; j++)
+                    {
+                        arrayCounters[j] = tempMaxCounter;
+                    }
+                }
+            }
+
+            return arrayCounters;
+        }
+    }
+}
